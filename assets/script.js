e98e44932117c95789595ca973b3d61dabc05f62
Jasmine Ulloa <ulloajazz6@gmail.com>
2022-12-11 22:25:52 -0800

    added Javascript to the Prework Study Guide

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+var topic = "HTML";
+
+if (topic === 'HTML') {
+ console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+ console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+ console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+ console.log("Let's study JavaScript!");
+} else {
+ console.log('Please try again!');
+}
+
+var topic = ["HTML", "CSS", "Git", "Javascript"];
+for(var x = 0; x < topic.length; x++) {
+    console.log(topic[x]);
+   }
